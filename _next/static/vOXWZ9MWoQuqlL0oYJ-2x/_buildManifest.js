66c430719abe90728951d9cbe63e8a4379384ce6
polyxx <polyxx@users.noreply.github.com>
2021-03-14 19:01:32 +0000

    deploy: bc8b3efc015ec204f8f5afccdc1e75f54b317080

--- _next/static/vOXWZ9MWoQuqlL0oYJ-2x/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fb32d1511c2fe04a7b382.css","static\u002Fchunks\u002Fpages\u002Findex-f771d8b2076110330d85.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-44170860e34af52a3c8b.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
