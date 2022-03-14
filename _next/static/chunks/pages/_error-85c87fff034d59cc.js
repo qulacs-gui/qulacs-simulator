49ed3ecb9b7ad0868c28460fcbeba011268a2a21
polyxx <polyxx@users.noreply.github.com>
2022-03-14 15:01:34 +0000

    deploy: d366dab36e7447dacdb3cfdad457b5dce25ad124

---
+++ _next/static/chunks/pages/_error-85c87fff034d59cc.js
@@ -0,0 +1,2 @@
+/*! For license information please see _error-85c87fff034d59cc.js.LICENSE.txt */
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[820],{4001:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return u(72918)}])}},function(n){n.O(0,[774,888,179],(function(){return _=4001,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
