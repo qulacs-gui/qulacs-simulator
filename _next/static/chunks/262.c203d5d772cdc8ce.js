e3ec25d255a6e7bcacc9f9682f0bafa28801878d
polyxx <polyxx@users.noreply.github.com>
2023-02-12 11:40:07 +0000

    deploy: f7b4b1558ef1783b05cac55304e55b964977b1d6

---
+++ _next/static/chunks/262.c203d5d772cdc8ce.js
@@ -0,0 +1,2 @@
+/*! For license information please see 262.c203d5d772cdc8ce.js.LICENSE.txt */
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[262],{40262:function(e,t,n){n.r(t),n.d(t,{CodeEditor:function(){return i}});var o=n(52322),s=(n(95095),n(40900),n(96110),n(2784),n(21291));const i=e=>(0,o.jsx)(s.ZP,{mode:"python",theme:"monokai",width:"100%",height:"500px",fontSize:14,...e,setOptions:{useWorker:!1}})}}]);
